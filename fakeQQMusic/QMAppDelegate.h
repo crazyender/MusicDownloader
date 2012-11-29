d2e49ad40d76840837a5c6042f8a71d725b0629b
user <user@Hackintosh.local>
2012-11-29 23:27:35 +0800

    Initial Commit

---
+++ fakeQQMusic/QMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QMAppDelegate.h
+//  fakeQQMusic
+//
+//  Created by user on 12-11-29.
+//  Copyright (c) 2012年 crazyender. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface QMAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
